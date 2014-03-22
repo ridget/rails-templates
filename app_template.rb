0f6d6eb21abc2e2a1db27d1ed73ad297d0eb32f3
ridget <tom.ridge@tworedkites.com>
2014-03-23 08:48:11 +1000

    second step

--- app_template.rb
+++ app_template.rb
@@ -1,1 +1,11 @@
-gem 'bootstrap-sass'
+gem_group :assets do
+  gem 'bootstrap-sass'
+end
+
+gem_group :development, :test do
+  gem 'shoulda-matchers', '~> 2.4.0'
+end
+
+inside app_name do
+  run 'bundle install'
+end
