5734489e2e4e54abacc8e67a15ada8e9ecefeff4
ridget <tom.ridge@tworedkites.com>
2014-03-23 10:06:30 +1000

    step 3

--- app_template.rb
+++ app_template.rb
@@ -1,9 +1,21 @@
+gem 'devise'
+gem 'cancan'
+gem 'haml-rails'
+gem 'mysql2'
+gem 'capistrano', '~> 2.0'
+
+
 gem_group :assets do
   gem 'bootstrap-sass'
 end
 
 gem_group :development, :test do
+  gem 'capybara'
+  gem 'factory_girl_rails'
   gem 'shoulda-matchers', '~> 2.4.0'
+  gem 'rspec-rails'
+  gem 'timecop'
+  gem 'zeus', '~> 0.13.4.pre', :require => false
 end
 
 inside app_name do
